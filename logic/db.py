b094ed2505bb1a0f18d9cc3be2588a612e1fa8e5
Mamosebetsi Kekae <69590144+shaymk1@users.noreply.github.com>
2026-01-12 15:55:29 +0200

    created a sql folder and 3 files in it as well as renamed the inventory folder to logic folder

---
+++ logic/db.py
@@ -0,0 +1,15 @@
+from enum import UNIQUE
+from tkinter.tix import INTEGER, TEXT
+
+
+cursor.execute(
+    """
+CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    role TEXT NOT NULL,   
+);
+
+"""
+)
